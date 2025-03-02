6cc10190c79c5f715fe634db21018a25aba5c4c5
Craig McQueen <craig.mcqueen@innerrange.com>
2025-03-02 12:50:34 +1100

    Initial functional userspace LED triggers
    It's a little rough but basic functionality works. I have tested OFF, ON
    and BLINK ioctl operations.

--- include/uapi/linux/uledtriggers.h
+++ include/uapi/linux/uledtriggers.h
@@ -15,10 +15,68 @@
 #ifndef _UAPI__ULEDTRIGGERS_H_
 #define _UAPI__ULEDTRIGGERS_H_
 
-#define LED_TRIGGER_MAX_NAME_SIZE	64
+// See TRIG_NAME_MAX in linux/leds.h
+#define LED_TRIGGER_MAX_NAME_SIZE	50
 
 struct uledtriggers_user_dev {
 	char name[LED_TRIGGER_MAX_NAME_SIZE];
 };
+
+struct uledtriggers_blink {
+	unsigned long delay_on;
+	unsigned long delay_off;
+};
+
+struct uledtriggers_blink_oneshot {
+	unsigned long delay_on;
+	unsigned long delay_off;
+	int invert;
+};
+
+
+/* ioctl commands */
+
+#define ULEDTRIGGERS_IOC_MAGIC			'T'
+
+
+/*
+ * Turn the trigger off.
+ * E.g.:
+ *	int retval;
+ *	retval = ioctl(fd, ULEDTRIGGERS_IOC_OFF);
+ */
+#define ULEDTRIGGERS_IOC_OFF		_IO(ULEDTRIGGERS_IOC_MAGIC, 0x10)
+
+/*
+ * Turn the trigger on.
+ * E.g.:
+ *	int retval;
+ *	retval = ioctl(fd, ULEDTRIGGERS_IOC_ON);
+ */
+#define ULEDTRIGGERS_IOC_ON		_IO(ULEDTRIGGERS_IOC_MAGIC, 0x11)
+
+/*
+ * Set the LED trigger to blink continuously.
+ * E.g.:
+ *	int retval;
+ *	struct uledtriggers_blink blink;
+ *      blink.delay_on = 100;
+ *      blink.delay_off = 400;
+ *	retval = ioctl(fd, ULEDTRIGGERS_IOC_BLINK, &blink);
+ */
+#define ULEDTRIGGERS_IOC_BLINK		_IOW(ULEDTRIGGERS_IOC_MAGIC,  0x12, struct uledtriggers_blink)
+
+/*
+ * Set the LED trigger to blink once.
+ * E.g.:
+ *	int retval;
+ *	struct uledtriggers_blink_oneshot blink_oneshot;
+ *      blink_oneshot.delay_on = 100;
+ *      blink_oneshot.delay_off = 400;
+ *      blink_oneshot.invert = false;
+ *	retval = ioctl(fd, ULEDTRIGGERS_IOC_BLINK_ONESHOT, &blink_oneshot);
+ */
+#define ULEDTRIGGERS_IOC_BLINK_ONESHOT	_IOW(ULEDTRIGGERS_IOC_MAGIC,  0x13, struct uledtriggers_blink_oneshot)
+
 
 #endif /* _UAPI__ULEDTRIGGERS_H_ */
--- uledtriggers.c
+++ uledtriggers.c
@@ -22,29 +22,70 @@ enum uledtriggers_state {
 	ULEDTRIGGERS_STATE_REGISTERED,
 };
 
+enum uledtriggers_trig_state {
+	TRIG_STATE_OFF,
+	TRIG_STATE_ON,
+	TRIG_STATE_BLINK,
+};
+
 struct uledtriggers_device {
 	struct uledtriggers_user_dev	user_dev;
 	struct led_classdev	led_cdev;
+	struct led_trigger	led_trigger;
 	struct mutex		mutex;
 	enum uledtriggers_state	state;
-	wait_queue_head_t	waitq;
+	enum uledtriggers_trig_state	trig_state;
+	unsigned long		trig_delay_on;
+	unsigned long		trig_delay_off;
 	int			brightness;
 	bool			new_data;
 };
 
 static struct miscdevice uledtriggers_misc;
 
-static void uledtriggers_brightness_set(struct led_classdev *led_cdev,
-				 enum led_brightness brightness)
+static char * trig_state_name(enum uledtriggers_trig_state trig_state)
 {
-	struct uledtriggers_device *udev = container_of(led_cdev, struct uledtriggers_device,
-						 led_cdev);
-
-	if (udev->brightness != brightness) {
-		udev->brightness = brightness;
-		udev->new_data = true;
-		wake_up_interruptible(&udev->waitq);
+	switch (trig_state) {
+		case TRIG_STATE_OFF:	return "off";
+		case TRIG_STATE_ON:	return "on";
+		case TRIG_STATE_BLINK:	return "blink";
+		default:		return "unknown";
 	}
+	return "unknown";
+}
+
+static void set_led_trigger(struct uledtriggers_device *data)
+{
+	dev_dbg(uledtriggers_misc.this_device, "LED trigger %s set to %s\n",
+			data->led_trigger.name,
+			trig_state_name(data->trig_state));
+	switch (data->trig_state) {
+	default:
+	case TRIG_STATE_OFF:
+		led_trigger_event(&data->led_trigger, LED_OFF);
+		break;
+	case TRIG_STATE_ON:
+		led_trigger_event(&data->led_trigger, LED_FULL);
+		break;
+	case TRIG_STATE_BLINK:
+		led_trigger_blink(&data->led_trigger, data->trig_delay_on, data->trig_delay_off);
+		break;
+	}
+}
+
+/*
+ * When an LED is connected to the trigger, this 'activate' function runs and
+ * sets the initial state of the LED.
+ */
+static int uledtriggers_trig_activate(struct led_classdev *led_cdev)
+{
+	struct led_trigger		*trig;
+	struct uledtriggers_device	*data;
+
+	trig = led_cdev->trigger;
+	data = container_of(trig, struct uledtriggers_device, led_trigger);
+	set_led_trigger(data);
+	return 0;
 }
 
 static int uledtriggers_open(struct inode *inode, struct file *file)
@@ -55,11 +96,10 @@ static int uledtriggers_open(struct inode *inode, struct file *file)
 	if (!udev)
 		return -ENOMEM;
 
-	udev->led_cdev.name = udev->user_dev.name;
-	udev->led_cdev.brightness_set = uledtriggers_brightness_set;
+	udev->led_trigger.name = udev->user_dev.name;
+	udev->led_trigger.activate = uledtriggers_trig_activate;
 
 	mutex_init(&udev->mutex);
-	init_waitqueue_head(&udev->waitq);
 	udev->state = ULEDTRIGGERS_STATE_UNKNOWN;
 
 	file->private_data = udev;
@@ -105,8 +145,7 @@ static ssize_t uledtriggers_write(struct file *file, const char __user *buffer,
 		goto out;
 	}
 
-	ret = devm_led_classdev_register(uledtriggers_misc.this_device,
-					 &udev->led_cdev);
+	ret = led_trigger_register(&udev->led_trigger);
 	if (ret < 0)
 		goto out;
 
@@ -120,55 +159,72 @@ out:
 	return ret;
 }
 
-static ssize_t uledtriggers_read(struct file *file, char __user *buffer, size_t count,
-			  loff_t *ppos)
+static long uledtriggers_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	struct uledtriggers_device *udev = file->private_data;
-	ssize_t retval;
+	struct uledtriggers_blink blink;
+	struct uledtriggers_blink_oneshot blink_oneshot;
+	int retval = 0;
 
-	if (count < sizeof(udev->brightness))
-		return 0;
+	/*
+	 * the direction is a bitmask, and VERIFY_WRITE catches R/W
+	 * transfers. `Direction' is user-oriented, while
+	 * access_ok is kernel-oriented, so the concept of "read" and
+	 * "write" is reversed
+	 */
+	retval = 0;
+	if (_IOC_DIR(cmd) & _IOC_READ)
+		retval = !access_ok((void __user *)arg, _IOC_SIZE(cmd));
+	else if (_IOC_DIR(cmd) & _IOC_WRITE)
+		retval =  !access_ok((void __user *)arg, _IOC_SIZE(cmd));
+	if (retval)
+		return -EFAULT;
 
-	do {
-		retval = mutex_lock_interruptible(&udev->mutex);
+	switch (cmd) {
+	case ULEDTRIGGERS_IOC_OFF:
+		udev->trig_delay_on = 0u;
+		udev->trig_delay_off = 0u;
+		udev->trig_state = TRIG_STATE_OFF;
+		led_trigger_event(&udev->led_trigger, LED_OFF);
+		break;
+
+	case ULEDTRIGGERS_IOC_ON:
+		udev->trig_delay_on = 0u;
+		udev->trig_delay_off = 0u;
+		udev->trig_state = TRIG_STATE_ON;
+		led_trigger_event(&udev->led_trigger, LED_FULL);
+		break;
+
+	case ULEDTRIGGERS_IOC_BLINK:
+		retval = copy_from_user(&blink,
+			(struct uledtriggers_blink __user *)arg,
+			sizeof(blink));
 		if (retval)
 			return retval;
+		udev->trig_delay_on = blink.delay_on;
+		udev->trig_delay_off = blink.delay_off;
+		udev->trig_state = TRIG_STATE_BLINK;
+		led_trigger_blink(&udev->led_trigger, blink.delay_on, blink.delay_off);
+		break;
 
-		if (udev->state != ULEDTRIGGERS_STATE_REGISTERED) {
-			retval = -ENODEV;
-		} else if (!udev->new_data && (file->f_flags & O_NONBLOCK)) {
-			retval = -EAGAIN;
-		} else if (udev->new_data) {
-			retval = copy_to_user(buffer, &udev->brightness,
-					      sizeof(udev->brightness));
-			udev->new_data = false;
-			retval = sizeof(udev->brightness);
-		}
-
-		mutex_unlock(&udev->mutex);
-
+	case ULEDTRIGGERS_IOC_BLINK_ONESHOT:
+		retval = copy_from_user(&blink_oneshot,
+			(struct uledtriggers_blink_oneshot __user *)arg,
+			sizeof(blink_oneshot));
 		if (retval)
-			break;
+			return retval;
+		udev->trig_delay_on = 0u;
+		udev->trig_delay_off = 0u;
+		udev->trig_state = blink_oneshot.invert ? TRIG_STATE_ON : TRIG_STATE_OFF;
+		led_trigger_blink_oneshot(&udev->led_trigger, blink_oneshot.delay_on, blink_oneshot.delay_off, blink_oneshot.invert);
+		break;
 
-		if (!(file->f_flags & O_NONBLOCK))
-			retval = wait_event_interruptible(udev->waitq,
-					udev->new_data ||
-					udev->state != ULEDTRIGGERS_STATE_REGISTERED);
-	} while (retval == 0);
+	default:
+		retval = -ENOIOCTLCMD;
+		break;
+	}
 
 	return retval;
-}
-
-static __poll_t uledtriggers_poll(struct file *file, poll_table *wait)
-{
-	struct uledtriggers_device *udev = file->private_data;
-
-	poll_wait(file, &udev->waitq, wait);
-
-	if (udev->new_data)
-		return EPOLLIN | EPOLLRDNORM;
-
-	return 0;
 }
 
 static int uledtriggers_release(struct inode *inode, struct file *file)
@@ -177,8 +233,7 @@ static int uledtriggers_release(struct inode *inode, struct file *file)
 
 	if (udev->state == ULEDTRIGGERS_STATE_REGISTERED) {
 		udev->state = ULEDTRIGGERS_STATE_UNKNOWN;
-		devm_led_classdev_unregister(uledtriggers_misc.this_device,
-					     &udev->led_cdev);
+		led_trigger_unregister(&udev->led_trigger);
 	}
 	kfree(udev);
 
@@ -189,9 +244,8 @@ static const struct file_operations uledtriggers_fops = {
 	.owner		= THIS_MODULE,
 	.open		= uledtriggers_open,
 	.release	= uledtriggers_release,
-	.read		= uledtriggers_read,
 	.write		= uledtriggers_write,
-	.poll		= uledtriggers_poll,
+	.unlocked_ioctl	= uledtriggers_ioctl,
 	.llseek		= no_llseek,
 };
 
