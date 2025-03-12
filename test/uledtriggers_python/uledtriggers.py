492605471f83e2181e5e8ff0490a71a7daf243b7
Craig McQueen <craig@mcqueen.au>
2025-03-12 19:01:41 +1100

    Simplify Python test code

--- test/uledtriggers_python/uledtriggers.py
+++ test/uledtriggers_python/uledtriggers.py
@@ -26,76 +26,58 @@ class IOCTL(EnumMissingUnknown, IntEnum):
     ULEDTRIGGERS_IOC_BLINK          = IOC(IOC_WRITE, ord('t'), 0x20, 16)
     ULEDTRIGGERS_IOC_BLINK_ONESHOT  = IOC(IOC_WRITE, ord('t'), 0x21, 24)
 
+
 def uledtriggers_register(names):
     uledtriggers_f = []
     for name in names:
         f = open(CHAR_DEV_NAME, 'r+b', buffering=0)
-        name_struct = struct.pack('@64s', name.encode('utf-8'))
+        setup_struct = struct.pack('@64s', name.encode('utf-8'))
         if False:
-            f.write(name_struct)
+            f.write(setup_struct)
         else:
-            fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_DEV_SETUP, name_struct)
+            fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_DEV_SETUP, setup_struct)
         uledtriggers_f.append((name, f))
     return uledtriggers_f
 
-def uledtriggers_blink(uledtriggers_f):
-    for i, (_name, f) in enumerate(uledtriggers_f):
-        blink_struct = struct.pack('@QQ', 1000, 2000)
-        fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_BLINK, blink_struct)
-    while True:
-        time.sleep(1)
-
-def uledtriggers_periodic_blink_oneshot(uledtriggers_f):
-    while True:
-        time.sleep(2)
-        for i, (_name, f) in enumerate(uledtriggers_f):
-            invert = 1 if i & 1 else 0
-            blink_struct = struct.pack('@QQIxxxx', 200, 300, invert)
-            fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_BLINK_ONESHOT, blink_struct)
-
-def uledtriggers_periodic_toggle_on_off(uledtriggers_f):
-    while True:
-        time.sleep(2)
-        for i, (_name, f) in enumerate(uledtriggers_f):
-            fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_ON if i & 1 else IOCTL.ULEDTRIGGERS_IOC_OFF)
-        time.sleep(2)
-        for i, (_name, f) in enumerate(uledtriggers_f):
-            fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_OFF if i & 1 else IOCTL.ULEDTRIGGERS_IOC_ON)
-
-def uledtriggers_periodic_toggle_event(uledtriggers_f):
-    while True:
-        time.sleep(2)
-        for i, (_name, f) in enumerate(uledtriggers_f):
-            brightness = 255 if i & 1 else 0
-            brightness_bytes = struct.pack('@i', brightness)
-            fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_EVENT, brightness_bytes)
-        time.sleep(2)
-        for i, (_name, f) in enumerate(uledtriggers_f):
-            brightness = 0 if i & 1 else 255
-            brightness_bytes = struct.pack('@i', brightness)
-            fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_EVENT, brightness_bytes)
-
-def uledtriggers_periodic_toggle_write(uledtriggers_f):
-    while True:
-        time.sleep(2)
-        for i, (_name, f) in enumerate(uledtriggers_f):
-            brightness = 255 if i & 1 else 0
-            brightness_bytes = struct.pack('@i', brightness)
-            f.write(brightness_bytes)
-        time.sleep(2)
-        for i, (_name, f) in enumerate(uledtriggers_f):
-            brightness = 0 if i & 1 else 255
-            brightness_bytes = struct.pack('@i', brightness)
-            f.write(brightness_bytes)
+def brightness_bytes(brightness=0):
+    return struct.pack('@i', brightness)
 
 def main():
     uledtriggers_f = uledtriggers_register(ledtrigger_names)
     try:
-        uledtriggers_periodic_toggle_on_off(uledtriggers_f)
-        #uledtriggers_periodic_toggle_event(uledtriggers_f)
-        #uledtriggers_periodic_toggle_write(uledtriggers_f)
-        #uledtriggers_blink(uledtriggers_f)
-        #uledtriggers_periodic_blink_oneshot(uledtriggers_f)
+        while True:
+            # Change brightness via write.
+            for i, (_name, f) in enumerate(uledtriggers_f):
+                f.write(brightness_bytes(255))
+            time.sleep(2)
+            for i, (_name, f) in enumerate(uledtriggers_f):
+                f.write(brightness_bytes(0))
+            time.sleep(2)
+
+            # Change brightness via ioctl.
+            for i, (_name, f) in enumerate(uledtriggers_f):
+                fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_EVENT, brightness_bytes(255))
+            time.sleep(1)
+            for i, (_name, f) in enumerate(uledtriggers_f):
+                fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_EVENT, brightness_bytes(0))
+            time.sleep(1)
+
+            # Set up continuous blink.
+            blink_struct = struct.pack('@QQ', 200, 200)
+            for i, (_name, f) in enumerate(uledtriggers_f):
+                fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_BLINK, blink_struct)
+            time.sleep(1)
+            for i, (_name, f) in enumerate(uledtriggers_f):
+                fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_OFF, 0)
+            time.sleep(1)
+
+            # Single blink
+            blink_struct = struct.pack('@QQIxxxx', 100, 200, 0)
+            for i, (_name, f) in enumerate(uledtriggers_f):
+                fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_OFF, 0)
+                fcntl.ioctl(f, IOCTL.ULEDTRIGGERS_IOC_BLINK_ONESHOT, blink_struct)
+            time.sleep(1)
+
     finally:
         for _name, f in uledtriggers_f:
             f.close()
